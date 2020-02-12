c517655554179d8b0c0b47cacf1d719e8ce4dde9
Blahclah <adrianswindle@amdg.rockhusths.edu>
2020-02-12 08:50:25 -0600

    Coin flip C stadge

--- Coinflip.py
+++ Coinflip.py
@@ -2,16 +2,26 @@ import random
 
 numFlips = int(input("How many flips? "))
 
-flips = [0,1]
+flips = [0]*2
 
 i = 0
 
 while (i< numFlips):
     flip = random.randint(0,1)
     flips[flip-1] +=1
+
+  
     i += 1
 
-print("0 = heads and 1 = tails")
+
+   
+
+    
+
 for i in  range(len(flips)):
     print(f"{i}: {flips[i]}/{numFlips}  = {flips[i]/numFlips*100}%")
-    
+
+
+
+
+
